d97ba653853094b4ce9cb0aead667173eaaed0f8
Andrew Dillon <AndrewDillon91@gmail.com>
2019-07-27 14:32:21 -0700

    Added facing and a slower, smoother lerp.

--- Project/Assets/PlayerCameraCameraController.cs
+++ Project/Assets/PlayerCameraCameraController.cs
@@ -5,20 +5,41 @@ using UnityEngine;
 public class PlayerCameraCameraController : MonoBehaviour
 {
     // Target to follow
-    public Transform Target;
+    public GameObject Target;
 
     public Vector3 CameraOffset;
     public float SmoothSpeed = 0.005f;
+    public float FacingOffset = 5.0f;
+
+    protected Transform TargetTransform;
+    protected MovementController TargetMovementController;
 
     void Start()
     {
-
+        if (Target)
+        {
+            TargetTransform = Target.GetComponent<Transform>();
+            TargetMovementController = Target.GetComponent<MovementController>();
+        }
     }
 
     void FixedUpdate()
     {
-        Vector3 DesiredPosition = Target.position + CameraOffset;
+        
+
+        // Create default desired position
+        Vector3 DesiredPosition = TargetTransform.position + CameraOffset;
+
+        if (TargetMovementController)
+        {
+            float Direction = TargetMovementController.IsFacingRight ? 1 : -1;
+            DesiredPosition.x += FacingOffset * Direction;
+        }
+
+        // Smooth to desired position using a lerp.
         Vector3 SmoothPosition = Vector3.Lerp(transform.position, DesiredPosition, SmoothSpeed);
+
+        // Update Camera Position
         transform.position = SmoothPosition;
     }
 }
