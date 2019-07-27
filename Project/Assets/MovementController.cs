6a7358b0fd3c796dc87e887fd37dd1b707ea9fee
Gabriel Chenier <reece.chenier@gmail.com>
2019-07-27 02:53:26 -0700

    Player & movement controller

---
+++ Project/Assets/MovementController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Require a MovementController
+[RequireComponent(typeof(Rigidbody))]
+public class MovementController : MonoBehaviour
+{
+  protected Rigidbody Rigid;
+
+  // Input manipulated by controllers such
+  // as the player or AI brain bois.
+  // We use this to determine movement based
+  // on other tunables.
+  public float HAcceleration = 100.0f;
+  public float HFriction = 10.0f;
+  public float Speed { get { return HAcceleration / HFriction; } }
+
+  protected Vector2 Input;
+  protected bool IsGrounded;
+
+  // 
+
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    Rigid = GetComponent<Rigidbody>();
+  }
+
+  public void TryJump()
+  {
+    if (IsGrounded)
+    {
+      Jump();
+    }
+  }
+
+  public void Jump()
+  {
+    // Jumpshitman
+  }
+
+  public void SetInput(Vector2 input)
+  {
+    Input = input;
+  }
+
+  // Update is called once per frame
+  void FixedUpdate()
+  {
+    Rigid.velocity = new Vector3(Rigid.velocity.x * (1.0f - (HFriction * Time.fixedDeltaTime)), Rigid.velocity.y, Rigid.velocity.z);
+    
+    float XAccel = HAcceleration * Input.x * Time.fixedDeltaTime;
+    float ZAccel = 0.0f;
+    Rigid.velocity += new Vector3(XAccel, 0.0f, ZAccel);
+
+  // Fuck you
+  Input = new Vector2();
+  }
+}
--- Project/Assets/PlayerController.cs
+++ Project/Assets/PlayerController.cs
@@ -2,17 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Require a MovementController
+[RequireComponent(typeof(MovementController))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+  MovementController MvCon;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    // Set the cache for our movement controller
+    MvCon = GetComponent<MovementController>();
+  }
+
+  float GetHorizontalMovement()
+  {
+    float input = 0.0f;
+
+    // Movement left
+    if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
     {
-        
+      input -= 1.0f;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Movement Right
+    if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
     {
-        
+      input += 1.0f;
     }
+    return input;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // Set the horizontal movement for the character
+    // based on user input.
+
+    // TODO: we may want to scale this over time?
+    MvCon.SetInput(new Vector2(GetHorizontalMovement(), 0.0f));
+
+  }
 }
