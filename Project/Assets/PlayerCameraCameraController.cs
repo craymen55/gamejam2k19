c7f05ce1c52e0333be9eaa5eccdcc8fbfce7bcab
Andrew Dillon <AndrewDillon91@gmail.com>
2019-07-27 14:02:40 -0700

    First pass of player controller.

---
+++ Project/Assets/PlayerCameraCameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraCameraController : MonoBehaviour
+{
+    // Target to follow
+    public Transform Target;
+
+    public Vector3 CameraOffset;
+    public float SmoothSpeed = 0.125f;
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 DesiredPosition = Target.position + CameraOffset;
+        Vector3 SmoothPosition = Vector3.Lerp(transform.position, DesiredPosition, SmoothSpeed);
+        transform.position = SmoothPosition;
+    }
+}
