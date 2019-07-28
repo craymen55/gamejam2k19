f8901dd7420dda22c856469d628539bd3c26ac06
Chase A Rayment <chase.rayment@digipen.edu>
2019-07-28 13:34:57 -0700

    Added CharacterSprite script.

---
+++ Project/Assets/CharacterSprite.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSprite : MonoBehaviour
+{
+  public Sprite[] Frames;
+
+  public SpriteRenderer SpriteRenderer;
+
+  void SetFrame(uint index)
+  {
+    if(SpriteRenderer != null)
+    {
+      if(index < Frames.Length)
+      {
+        SpriteRenderer.sprite = Frames[index];
+      }
+      else
+      {
+        Debug.LogError("Frame index " + name + " is out of bounds!");
+      }
+    }
+    else
+    {
+      Debug.LogError("Tried to use a CharacterSprite without an assigned SpriteRenderer!");
+      enabled = false;
+    }
+
+  }
+}
